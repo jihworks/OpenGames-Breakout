0976f5c22501ec4f7c3b84680bfe8d382ba6b2da
Jong-il Hong <86797396+jihworks@users.noreply.github.com>
2026-02-27 10:11:01 +0900

    Infra making

---
+++ OpenGames-Breakout/Assets/Plugins/OpenGames/Source/EnumerableEx.cs
@@ -0,0 +1,33 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Jih.OpenGames
+{
+    public static class EnumerableEx
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsValidIndex<T>(this IReadOnlyList<T> collection, int index)
+        {
+            return 0 <= index && index < collection.Count;
+        }
+
+        public static bool TryGetValueAt<T>(this IReadOnlyList<T> collection, int index, out T? value)
+        {
+            if (collection.IsValidIndex(index))
+            {
+                value = collection[index];
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+    }
+}
